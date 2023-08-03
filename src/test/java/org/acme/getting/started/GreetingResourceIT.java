7e1baabb74dfd5636d113b17a72cefedbbf308b2
James Land <hokie10@gmail.com>
2023-08-03 12:15:13 -0400

    Branch for the inital run

--- src/test/java/org/acme/getting/started/GreetingResourceIT.java
+++
@@ -1,9 +0,0 @@
-package org.acme.getting.started;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-public class GreetingResourceIT extends GreetingResourceTest {
-
-    // Execute the same tests but in native mode.
-}
